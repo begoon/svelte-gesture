0fa5950e5e20df6b6afa48fa06d6866e3f01510f
Alexander Demin <ademin@gmail.com>
2023-06-18 17:43:29 +0100

    Swiper

--- lib/components/App.svelte
+++ lib/components/App.svelte
@@ -1,50 +1,108 @@
 <script>
-    import { pan, swipe } from "svelte-gestures";
-    let x;
-    let y;
-    let target;
-
-    function handlerPan(event) {
-        x = event.detail.x;
-        y = event.detail.y;
-        target = event.detail.target;
-    }
-
+    import { swipe } from "svelte-gestures";
     let direction;
     function handlerSwipe(event) {
         direction = event.detail.direction;
+        switch (direction) {
+            case "left":
+                if (--cx < 0) cx = mx - 1;
+                break;
+            case "right":
+                if (++cx >= mx) cx = 0;
+                break;
+            case "top":
+                if (--cy < 0) cy = my - 1;
+                break;
+            case "bottom":
+                if (++cy >= my) cy = 0;
+                break;
+        }
     }
+
+    import { files } from "./files.js";
+
+    const images = files();
+
+    const mx = images[0].length;
+    let cx = Math.floor(mx / 2);
+
+    const my = images.length;
+    let cy = Math.floor(my / 2);
 </script>
 
-Pan
-<div use:pan={{ delay: 300 }} on:pan={handlerPan}>
-    pan: {x}
-    {y}
-</div>
-
-Swipe
-<div
-    use:swipe={{ timeframe: 300, minSwipeDistance: 100, touchAction: "pan-y" }}
-    on:swipe={handlerSwipe}
->
-    This one <b>swipes only in horizontal directions</b>:
-    <span style="color: red;">{direction}</span><br />
-    You can scroll vertically as normal. It is due to
-    <b>touchAction: 'pan-y'</b>
+<div class="action">
+    {#if direction}
+        Действие:
+        <span style="color: red;">{direction}</span>
+    {/if}
 </div>
 
 <div
+    class="swiper"
     use:swipe={{ timeframe: 300, minSwipeDistance: 100 }}
     on:swipe={handlerSwipe}
+    style="justify-content: center; align-items: center;"
 >
-    This <b>swipes in all directions</b>:
-    <span style="color:red;">{direction}</span>
+    <table>
+        <tr><td colspan="3">&uarr;</td></tr>
+        <tr>
+            <td>&larr;</td>
+            <td />
+            <td>&rarr;</td>
+        </tr><tr><td colspan="3">&darr;</td></tr>
+    </table>
 </div>
 
+{#each images as names, y}
+    <ul>
+        {#each names as name, x}
+            {@const current = x === cx && y === cy}
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <img src={name} class="icon" class:current />
+        {/each}
+    </ul>
+    <!-- svelte-ignore a11y-missing-attribute -->
+{/each}
+<!-- svelte-ignore a11y-missing-attribute -->
+<img src={images[cy][cx]} class="preview" />
+
 <style>
-    div {
-        width: 500px;
-        height: 100px;
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    div.action {
+        display: block;
+        height: 1.5em;
+    }
+    div.swiper {
+        width: 100%;
+        height: 300px;
+        box-sizing: border-box;
         border: 1px solid black;
+    }
+    table {
+        width: 100%;
+        height: 100%;
+        border-collapse: collapse;
+    }
+    td {
+        text-align: center;
+        vertical-align: middle;
+        font-size: 70px;
+    }
+    img.preview {
+        height: 200px;
+        width: auto;
+        border: 1px solid black;
+    }
+    img.icon {
+        display: inline-block;
+        width: 30px;
+        height: auto;
+        margin: 2px;
+    }
+    img.current {
+        outline: 2px solid red;
     }
 </style>
